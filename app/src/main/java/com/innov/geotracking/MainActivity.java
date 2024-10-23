5fd180291a27bbc1497dd972fa27fef9880f6a86
khurseeda <khurseeda@innov.in>
2024-10-23 16:49:39 +0530

    init
    init

---
+++ app/src/main/java/com/innov/geotracking/MainActivity.java
@@ -0,0 +1,28 @@
+package com.innov.geotracking;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.innov.geo_tracking.EmailValidate;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText email = findViewById(R.id.edit_text);
+        Button validate = findViewById(R.id.button);
+        validate.setOnClickListener(v -> {
+            if (EmailValidate.getInstance().isValid(String.valueOf(email.getText())))
+                Toast.makeText(MainActivity.this, getString(R.string.valid), Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(MainActivity.this, getString(R.string.invalid), Toast.LENGTH_SHORT).show();
+        });
+    }
+}
